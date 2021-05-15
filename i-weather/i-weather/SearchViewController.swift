8542a6a92c31daee5a7c3919a456a8da937a17b1
RP <rahulpahuja2015@gmail.com>
2021-05-16 02:17:30 +0530

    Added Class for Both VC's

---
+++ i-weather/i-weather/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.swift
+//  i-weather
+//
+//  Created by Rahul Pahuja on 16/05/21.
+//
+
+import UIKit
+
+class SearchViewController:UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
