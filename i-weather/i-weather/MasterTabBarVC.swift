20383f91c4fdbdc80310a332d81b6b5ac7b36f35
RP <rahulpahuja2015@gmail.com>
2021-05-16 02:14:49 +0530

    Added Tab Bar VC

---
+++ i-weather/i-weather/MasterTabBarVC.swift
@@ -0,0 +1,13 @@
+//
+//  MasterTabBarVC.swift
+//  i-weather
+//
+//  Created by Rahul Pahuja on 16/05/21.
+//
+
+import Foundation
+import UIKit
+
+class MasterTabBarVC: UITabBarController,UITabBarControllerDelegate{
+    
+}
